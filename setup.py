339e07718ae9961ea8d2f9610a9362f1c5087af8
bibby <bibby@bbby.org>
2016-08-02 20:53:42 -0700

    increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme:
 
 setup(
     name='itch',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     description='Twitch APIs client',
     long_description=long_description,
